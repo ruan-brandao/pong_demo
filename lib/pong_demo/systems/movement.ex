83875604b3f112eb09b51092b80f11b088d9fd1b
Ruan Brandão <ruan.bernardo@gmail.com>
2024-05-25 15:43:56 -0300

    Fix paddle going over bottle of the screen

--- lib/pong_demo/systems/movement.ex
+++ lib/pong_demo/systems/movement.ex
@@ -8,6 +8,7 @@ defmodule PongDemo.Systems.Movement do
   alias PongDemo.Components.YPosition
   alias PongDemo.Components.XVelocity
   alias PongDemo.Components.YVelocity
+  alias PongDemo.Components.YSize
 
   @game_world_width Application.compile_env(:pong_demo, :game_world_width)
   @game_world_height Application.compile_env(:pong_demo, :game_world_height)
@@ -15,7 +16,6 @@ defmodule PongDemo.Systems.Movement do
   @impl ECSx.System
   def run do
     max_x_position = @game_world_width - 1
-    max_y_position = @game_world_height - 1
 
     for {entity, x_velocity} <- XVelocity.get_all() do
       new_x_position =
@@ -28,6 +28,8 @@ defmodule PongDemo.Systems.Movement do
 
     # Once the x-values are updated, do the same for the y-values
     for {entity, y_velocity} <- YVelocity.get_all() do
+      max_y_position = get_max_y_position(entity)
+
       new_y_position =
         entity
         |> YPosition.get()
@@ -43,5 +45,11 @@ defmodule PongDemo.Systems.Movement do
     new_position = current_position + velocity
     new_position = Enum.min([new_position, max_position])
     Enum.max([new_position, 0])
+  end
+
+  defp get_max_y_position(entity) do
+    entity_size = YSize.get(entity, 1) - 1
+
+    @game_world_height - 1 - entity_size
   end
 end
